d3f51ed0f7abcf1854ce1b6618a825aaff66b7e1
Akila Sachintha <akilasachintha@gmail.com>
2023-09-23 17:37:22 +0530

    moved assets folder into src and added path alise

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import MyButton from "@components/buttons/MyButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+        <MyButton/>
+        <Image source={require('@assets/icon.png')} style={{width: 200, height: 200}}/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/buttons/MyButton.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export default function MyButton() {
+    return (
+        <View>
+            <Text>My Button</Text>
+        </View>
+    );
+}
